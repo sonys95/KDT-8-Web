5586ced202ada80f57a40eddfe55c13657cdc835
sonys95 <sonys95@naver.com>
2023-08-22 21:59:00 +0900

    230822_MVC

---
+++ nodejs/230807_mvc_prac/14_mvc/model/Model.js
@@ -0,0 +1,60 @@
+const comments = [
+  {
+    id: 1,
+    userid: "hello",
+    date: "2023-08-01",
+    comment: "안녕하세요",
+  },
+  {
+    id: 2,
+    userid: "happy",
+    date: "2023-08-05",
+    comment: "반갑습니다",
+  },
+  {
+    id: 3,
+    userid: "lucky",
+    date: "2023-08-17",
+    comment: "행복하세요",
+  },
+  {
+    id: 4,
+    userid: "good",
+    date: "2023-08-20",
+    comment: "좋은 저녁되세요",
+  },
+];
+const users = [
+  {
+    id: 1,
+    name: "son",
+    gender: "M",
+    major: "sports",
+  },
+  {
+    id: 2,
+    name: "kim",
+    gender: "W",
+    major: "Eng",
+  },
+  {
+    id: 3,
+    name: "LEE",
+    gender: "W",
+    major: "MEC",
+  },
+  {
+    id: 4,
+    name: "KT",
+    gender: "M",
+    major: "PHY",
+  },
+  {
+    id: 5,
+    name: "son",
+    gender: "M",
+    major: "SYT",
+  },
+];
+
+module.exports = users;
