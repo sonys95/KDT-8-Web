92df35d4f53145484c8514e2fb504b9102282b81
sonys95 <sonys95@naver.com>
2023-10-10 22:10:12 +0900

    231010

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import PropsType1 from "./PropsType1";
+
+function App() {
+  return (
+    <>
+      <PropsType1 name={"typescript"} />
+    </>
+  );
+}
+
+export default App;
---
+++ react-typescript/src/PropsType1.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from "react";
+
+interface Props {
+  name: string | number;
+  age?: number;
+}
+
+const PropsType1: React.FC<Props> = ({ name, age }) => {
+  const [count, setCount] = useState<number | string>(0);
+  const myInput = useRef<HTMLInputElement>();
+
+  const handleFocus = () => {
+    myInput.current?.focus();
+  };
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {};
+  return (
+    <>
+      <h2>Hello {name}</h2>
+      <h4>{age}</h4>
+      <input ref={myInput} />
+      <button onClick={handleFocus}>버튼</button>
+    </>
+  );
+};
+
+export default PropsType1;
+
+// export default function ProsTye1({name} : Props) {
+//   return;
+//   <>
+//     <h2>Hellow {name} </h2>
+//   </>;
+// }
