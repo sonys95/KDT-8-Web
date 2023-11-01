c8393b4c5d77d0e886349ab41d08269107c2b400
sonys95 <sonys95@naver.com>
2023-11-01 16:03:41 +0900

    231101

--- react_preactice/src/App.js
+++ react_preactice/src/App.js
@@ -125,8 +125,8 @@ function App() {
   const [id, setId] = useState(null);
   const [nextId, setNextId] = useState(4);
   const [topics, setTopics] = useState([
-    { id: 1, title: "html", body: "html is ..." },
-    { id: 2, title: "css", body: "css is ..." },
+    { id: 1, title: "html", body: "html is ...." },
+    { id: 2, title: "css", body: "css is ...." },
     { id: 3, title: "javascript", body: "javascript is ..." },
   ]);
   let content = null;
