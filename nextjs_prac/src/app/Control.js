b519cc83ae43cb225f4c2b02ecc9199f88dd6612
sonys95 <sonys95@naver.com>
2023-11-13 22:33:29 +0900

    231113

---
+++ nextjs_prac/src/app/Control.js
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+
+export function Control() {
+  const params = useParams();
+  const router = useRouter();
+  const id = params.id;
+  return (
+    <ul>
+      <li>
+        <Link href="/create">create</Link>
+      </li>
+
+      {id ? (
+        <>
+          <li>
+            <Link href={"/update/" + id}>update</Link>
+          </li>
+          <li>
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+                fetch("http://localhost:9999/topics/" + id, options)
+                  .then((resp) => resp.json())
+                  .then((result) => {
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}
+            ></input>
+          </li>
+        </>
+      ) : null}
+    </ul>
+  );
+}
